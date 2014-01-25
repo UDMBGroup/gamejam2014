74ce6e94fa7dd39880fd0389b3c6f9826954f603
dylanhellems <dylanhellems@gmail.com>
2014-01-25 14:54:03 -0500

    Changes to Character

--- gamejam2014/src/edu/virginia/cs/sgd/game/model/Character.java
+++ gamejam2014/src/edu/virginia/cs/sgd/game/model/Character.java
@@ -12,12 +12,14 @@ public class Character {
 	private int charAssignment;
 	private Point pos;
 	private Map<String, Integer> isCollected;
+	private Map<String, Integer> isShown;
 	
 	public Character() {
 		this.name = "";
 		this.charAssignment = -1;
 		this.pos = null;
 		this.isCollected = null;
+		this.isShown = null;
 	}
 	
 	public Character(String name, int charAssignment, Point pos, ArrayList<String> evidenceList) {
@@ -25,6 +27,10 @@ public class Character {
 		this.charAssignment = charAssignment;
 		this.pos = pos;
 		this.isCollected = new HashMap<String, Integer>();
+		this.isShown = new HashMap<String, Integer>();
+		for (String s: evidenceList) {
+			isCollected.put(s, 0);
+		}
 		for (String s: evidenceList) {
 			isCollected.put(s, 0);
 		}
@@ -35,11 +41,27 @@ public class Character {
 	}
 	
 	public void setCollected(String evidence) {
-		isCollected.put(evidence, 1);
+		if (isCollected.get(evidence) == 0) {
+			isCollected.put(evidence, 1);
+		} else {
+			isCollected.put(evidence,  0);
+		}
 	}
 	
 	public boolean getIsCollected(String evidence) {
 		return isCollected.get(evidence) == 1;
+	}
+	
+	public void setShown(String evidence) {
+		if (isShown.get(evidence) == 0) {
+			isShown.put(evidence, 1);
+		} else {
+			isShown.put(evidence,  0);
+		}
+	}
+	
+	public boolean getIsShown(String evidence) {
+		return isShown.get(evidence) == 1;
 	}
 
 }
