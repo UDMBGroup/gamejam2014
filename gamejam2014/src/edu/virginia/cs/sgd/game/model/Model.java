aab094f11267bd89cb12c82875b67ea8183aff8b
dylanhellems <dylanhellems@gmail.com>
2014-01-25 11:58:26 -0500

    Model class added
    yah

---
+++ gamejam2014/src/edu/virginia/cs/sgd/game/model/Model.java
@@ -0,0 +1,31 @@
+package edu.virginia.cs.sgd.game.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.badlogic.gdx.graphics.g2d.tiled.TiledMap;
+
+public class Model {
+
+	private TiledMap map;
+	private ArrayList<String> evidenceList;
+	private Map<String, ArrayList<String>> evidenceTable;
+
+	public Model(TiledMap map) {
+		this.map = map;
+		evidenceList = new ArrayList<String>();
+		evidenceTable = new HashMap<String, ArrayList<String>>();
+		
+//		evidenceList.add("");
+		
+//		for (String ev; evidenceList) {
+//			evidenceTable.put(ev, )
+//		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
